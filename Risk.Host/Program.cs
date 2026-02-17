c9b659a42043934f5c0593aabae98366541ed551
Flavio Giacchetti <flv.giacchetti@gmail.com>
2026-02-17 17:36:11 +0100

    Allow LAN dev clients via permissive CORS policy

--- Risk.Host/Program.cs
+++ Risk.Host/Program.cs
@@ -12,9 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(
         "RiskWebDevClient",
         policy => policy
-            .WithOrigins(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173")
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
